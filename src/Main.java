c934435e18dcb9a2bc5feef57331174b1ae04c1d
ChefenML <milo1000@stud.ek.dk>
2026-02-04 16:05:20 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
+public class Main {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        double cm;
+        String name;
+        int age, feet, inches, remainder;
+        final double CM_PER_INCH = 2.54;
+        final int IN_PER_FOOT = 12;
+        Scanner in = new Scanner(System.in);
+
+        // prompt the user and get the value
+
+
+        System.out.println("What year would you like to look up?");
+
+        int year;
+        for (year = in.nextInt(); year % 4 == 0; ) {
+            System.out.print(year + " is leap-year");
+        }
+        System.out.print(year + " is not leap-year");
     }
 }
---
+++ src/TriangleTest.java
@@ -0,0 +1,22 @@
+package Lesson04.Costructor.Triangle;//package Lesson04.Costructor.Triangle;
+
+public class TriangleTest {
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle(3, 4, 5);
+        t1.showInfo();
+
+        System.out.println();
+
+        Triangle t2 = new Triangle(6, 6, 6);
+        t2.showInfo();
+        double areal1 = t1.getArea();
+        double areal2 = t2.getArea();
+
+        System.out.println("Hvilken er størst Trekant#1 eller Trekant#2");
+        if (areal1 > areal2) {
+            System.out.println("T1 er større!");
+        } else {
+            System.out.println("T2 er Større!");
+        }
+    }
+}
